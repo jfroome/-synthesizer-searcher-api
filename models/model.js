985d414cb588460a70b5099a77a31aa7a7c92a73
jfroome <froo0014@algonquinlive.com>
2022-11-15 20:50:22 -0500

    model built

--- models/model.js
+++ models/model.js
@@ -1,14 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const dataSchema = new mongoose.Schema({
-    name: {
-        required: true,
-        type: String
-    },
-    age: {
-        required: true,
-        type: Number
-    }
+  title: {
+    required: true,
+    type: String
+  },
+  price: {
+    required: true,
+    type: Number
+  },
+  currency: {
+    required: true,
+    type: String
+  },
+  site: {
+    required: true,
+    type: String
+  },
+  url: {
+    required: true,
+    type: String
+  },
+  date_posted: {
+    required: true,
+    type: Date
+  },
+  date_recorded: {
+    type: Date,
+    default: Date.now
+  },
+  date_updated: {
+    type: Date,
+    default: Date.now
+  },
+  tags: { type: [String], index: true }
 })
 
-module.exports = mongoose.model('Data', dataSchema);
+module.exports = mongoose.model('Listing', dataSchema)
