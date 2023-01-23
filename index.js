b153bf33eac46199ac1fb6111694550b5864b45e
jfroome <froo0014@algonquinlive.com>
2023-01-23 10:03:52 -0500

    added docker config

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ database.once('connected', () => {
 const app = express();
 app.use(express.json());
 app.use('/api', routes);
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server Started at ${process.env.PORT}`);
 });
 
